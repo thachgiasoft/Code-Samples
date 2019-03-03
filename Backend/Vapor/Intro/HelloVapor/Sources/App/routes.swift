7e845fea775d615bec9e0cbdd8cf7316deb73144
Tikhonov Alexander <uni.nova.tih@gmail.com>
2019-03-03 21:36:57 +0100

    HelloVapor

---
+++ Backend/Vapor/Intro/HelloVapor/Sources/App/routes.swift
@@ -0,0 +1,41 @@
+import Vapor
+
+/// Register your application's routes here.
+public func routes(_ router: Router) throws {
+    // Basic "It works" example
+    router.get { req in
+        return "It works!"
+    }
+    
+    // Basic "Hello, world!" example
+    router.get("hello") { req in
+        return "Hello, world!"
+    }
+
+    router.get("hello", "vapor") { req -> String in
+        return "Hello Vapor!"
+    }
+    
+    router.get("hello", String.parameter) { req -> String in
+        let name  = try req.parameters.next(String.self)
+        return "Hello, \(name)"
+    }
+    
+    router.post(InfoData.self, at: "info") { (req, data) -> InfoResponse in
+        return InfoResponse(request: data)
+    }
+    
+    // Example of configuring a controller
+    let todoController = TodoController()
+    router.get("todos", use: todoController.index)
+    router.post("todos", use: todoController.create)
+    router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+struct InfoData: Content {
+    let name: String
+}
+
+struct InfoResponse: Content {
+    let request: InfoData
+}
