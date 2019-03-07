9e7189fe455fb628b0f9f826e3aaad79178bf288
Tikhonov Alexander <uni.nova.tih@gmail.com>
2019-03-07 22:27:12 +0100

    loadView

---
+++ UI/Layout/loadView/loadView/loadView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  loadView
+//
+//  Created by atikhonov on 07.03.19.
+//  Copyright © 2019 atikhonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
