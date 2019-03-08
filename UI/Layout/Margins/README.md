ec6de05d529aa19acb725ac6c9e71ab43d0521d9
tikhonov <atikhonov@devexperts.com>
2019-03-08 13:54:37 +0100

    SafeArea root view example

---
+++ UI/Layout/Margins/README.md
@@ -0,0 +1,8 @@
+# Margins
+
+The constrain to margins checkbox is a little confusing. When you create a constraint to a view controller’s root view Interface Builder uses the safe area by default and the checkbox has no effect.
+
+![](SafeArea Constaint to margins ON.png)
+![](SafeArea Constaint to margins OFF.png)
+![](View Constaint to margins ON.png)
+![](View Constaint to margins OFF.png)
---
+++ UI/Layout/Margins/RootViewMargins/RootViewMargins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RootViewMargins
+//
+//  Created by Aleksandr Tikhonov on 08.03.19.
+//  Copyright © 2019 tikhonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
