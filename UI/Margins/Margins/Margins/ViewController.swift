1d73e4fb115bd98e26749894b90720fc41368077
Tikhonov Alexander <uni.nova.tih@gmail.com>
2019-03-10 16:06:07 +0100

    Safe Area relative margins sample

---
+++ UI/Margins/Margins/Margins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Margins
+//
+//  Created by atikhonov on 10.03.19.
+//  Copyright © 2019 atikhonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ UI/Margins/README.md
@@ -0,0 +1,8 @@
+# Margins
+
+
+## Safe Area relative margins ON
+![](Safe Area relative margins ON.png)
+
+## Safe Area relative margins OFF
+![](Safe Area relative margins OFF.png)
