c8a29e8229cb9944084d21259a74c88247ee3a61
Tikhonov Alexander <uni.nova.tih@gmail.com>
2019-03-10 12:36:13 +0100

    safe area vs margins

---
+++ UI/SaveArea/README.md
@@ -0,0 +1,10 @@
+# SafeArea
+
+## SafeArea portrait
+![](safe port.png)
+## Margins portrait
+![](margins port.png)
+## SafeArea landscape
+![](safe land.png)
+## Margins landscape
+![](margins land.png)
--- UI/SaveArea/SaveArea/SaveArea/ViewController.swift
+++ UI/SaveArea/SaveArea/SaveArea/ViewController.swift
@@ -20,20 +20,49 @@ class ViewController: UIViewController {
         
         let safeAreaLayoutGuide = view.safeAreaLayoutGuide
         
+        let rootViewSafeAreaButton = UIButton(type: .system)
+        let rootViewMarginsButton = UIButton(type: .system)
+        
+        rootViewSafeAreaButton.translatesAutoresizingMaskIntoConstraints = false
+        rootViewSafeAreaButton.setTitle("Safe Area example", for: .normal)
+        rootViewSafeAreaButton.addTarget(self, action: #selector(rootViewSafeAreaButtonTapped(sender:)), for: .touchUpInside)
+        redView.addSubview(rootViewSafeAreaButton)
+        
+        rootViewMarginsButton.translatesAutoresizingMaskIntoConstraints = false
+        rootViewMarginsButton.setTitle("Margins example", for: .normal)
+        rootViewMarginsButton.addTarget(self, action: #selector(rootViewMarginsButtonTapped(sender:)), for: .touchUpInside)
+        redView.addSubview(rootViewMarginsButton)
+        
         NSLayoutConstraint.activate(
             [
                 redView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
                 redView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
                 redView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
-                redView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+                redView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+                
+                rootViewSafeAreaButton.centerXAnchor.constraint(equalToSystemSpacingAfter: redView.centerXAnchor, multiplier: 1.0),
+                rootViewSafeAreaButton.centerYAnchor.constraint(equalTo: redView.centerYAnchor, constant: -20.0),
+                
+                
+                rootViewMarginsButton.centerXAnchor.constraint(equalToSystemSpacingAfter: redView.centerXAnchor, multiplier: 1.0),
+                rootViewMarginsButton.centerYAnchor.constraint(equalTo: redView.centerYAnchor, constant: 20.0)
             ])
         
         print("safeAreaInsets: ", view.safeAreaInsets)
         let customBarHeight: CGFloat = 50.0
         additionalSafeAreaInsets = UIEdgeInsets(top: customBarHeight, left: 0, bottom: 0, right: 0)
         print("additionalSafeAreaInsets: ", additionalSafeAreaInsets)
+        
+        
     }
 
+    @objc func rootViewSafeAreaButtonTapped(sender: UIButton) {
+        print(#function)
+    }
+    
+    @objc func rootViewMarginsButtonTapped(sender: UIButton) {
+        print(#function)
+    }
 
 }
 
