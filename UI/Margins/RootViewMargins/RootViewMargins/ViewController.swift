00aa46039676e7337783beb42eb16dcc1c7bb06c
Tikhonov Alexander <uni.nova.tih@gmail.com>
2019-03-12 09:08:51 +0100

    viewRespectsSystemMinimumLayoutMargins

--- UI/Margins/RootViewMargins/RootViewMargins/ViewController.swift
+++ UI/Margins/RootViewMargins/RootViewMargins/ViewController.swift
@@ -12,7 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(systemMinimumLayoutMargins)
+        viewRespectsSystemMinimumLayoutMargins = false
+        view.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0)
     }
 
 
