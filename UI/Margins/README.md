f5736efbc47d74b035e67d20b404a1c00040802c
Tikhonov Alexander <uni.nova.tih@gmail.com>
2019-03-12 21:29:06 +0100

    added one more example of layoutMarginsGuide

--- UI/Margins/README.md
+++ UI/Margins/README.md
@@ -1,6 +1,14 @@
 # Margins
 
 
+Unlike other views, the **system** manages the margins of a view controller’s root view.
+
+Starting in iOS 11 you can change the layout margins of the root view and control whether the system enforces a minimum margin.
+
+## View Respects System Minimum Layout Margins
+
+![](View Respects System Minimum Layout Margins.png)
+
 ## Safe Area relative margins ON
 ![](Safe Area relative margins ON.png)
 
--- UI/Margins/RootViewMargins/RootViewMargins/ViewController.swift
+++ UI/Margins/RootViewMargins/RootViewMargins/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    var yellowView: UIView = {
+        let v = UIView()
+        v.backgroundColor = .yellow
+        v.translatesAutoresizingMaskIntoConstraints = false
+        return v
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        redView.addSubview(yellowView)
+        redView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 20.0, leading: 5.0, bottom: 0.0, trailing: 5.0)
+        let layoutMarginsGuide = redView.layoutMarginsGuide
+        NSLayoutConstraint.activate([
+            yellowView.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            yellowView.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
+            yellowView.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+            yellowView.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor)
+            ])
         
     }
     
