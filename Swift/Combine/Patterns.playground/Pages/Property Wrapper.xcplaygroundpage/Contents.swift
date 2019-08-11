0432838bec18004bf27bdc0494c6e256c28d40a3
atikhonov <uni.nova.tih@gmail.com>
2019-08-11 13:48:22 +0200

    any cancable

--- Swift/Combine/Patterns.playground/Pages/Property Wrapper.xcplaygroundpage/Contents.swift
+++ Swift/Combine/Patterns.playground/Pages/Property Wrapper.xcplaygroundpage/Contents.swift
@@ -34,13 +34,14 @@ print(UserDefaultsConfig.firstLanch)
 final class FormViewController: UIViewController {
     
     @Published var isSubmitAllowed: Bool = false
+    private var switchedSubscriber: AnyCancellable?
     
     @IBOutlet private weak var acceptTermsSwitch: UISwitch!
     @IBOutlet private weak var submitButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        $isSubmitAllowed.receive(on: DispatchQueue.main).assign(to: \.isEnabled, on: submitButton)
+        switchedSubscriber = $isSubmitAllowed.receive(on: DispatchQueue.main).assign(to: \.isEnabled, on: submitButton)
     }
     
     @IBAction func didSwitch(_ sender: UISwitch) {
