bf997ebf25434c00e0f035947f1dd635ec4afc03
atikhonov <uni.nova.tih@gmail.com>
2019-08-21 20:36:12 +0200

    apple tutorial #4.1

---
+++ UI/SwiftUI/Landmarks Drawing paths and shapes/Landmarks/Supporting Views/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by atikhonov on 21.08.2019.
+//  Copyright © 2019 atikhonov. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        BadgeBackground()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
