35c039aa3e229f7681b25f8731ea25a7b68d5c98
atikhonov <uni.nova.tih@gmail.com>
2019-12-10 14:43:03 +0100

    filtering-operators

---
+++ Swift/Combine/filtering-operators/Challenge.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+import Combine
+
+var subscriptions = Set<AnyCancellable>()
+
+example(of: "Challenge") {
+    let numbers = (1...100).publisher
+    
+    numbers
+        .dropFirst(50)
+        .prefix(20)
+        .filter { $0.isMultiple(of: 2) }
+        .sink(receiveCompletion: { print("Completed with: \($0)") }, receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
